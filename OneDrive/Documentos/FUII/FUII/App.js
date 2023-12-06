8b5ed531e6c6a71d18fb0cbee5caf48c36986dd8
Lucas Víctor Ferreira de Araújo <120221548+Lucas13032003@users.noreply.github.com>
2023-12-06 19:19:08 -0300

    Primeira_tela

---
+++ OneDrive/Documentos/FUII/FUII/App.js
@@ -0,0 +1,71 @@
+import React, { useState } from 'react';
+import { View, Text, TextInput, TouchableOpacity, StyleSheet } from 'react-native';
+
+const LoginScreen = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleLogin = () => {
+    // Aqui você pode adicionar lógica de autenticação
+    console.log('Username:', username);
+    console.log('Password:', password);
+    // Adicione aqui a lógica de autenticação real (por exemplo, com API)
+  };
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Login</Text>
+
+      <TextInput
+        style={styles.input}
+        placeholder="Emai-intitucional"
+        onChangeText={(text) => setUsername(text)}
+      />
+
+      <TextInput
+        style={styles.input}
+        placeholder="Senha"
+        secureTextEntry={true}
+        onChangeText={(text) => setPassword(text)}
+      />
+
+      <TouchableOpacity style={styles.button} onPress={handleLogin}>
+        <Text style={styles.buttonText}>Entrar</Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#E3D3E8',
+  },
+  title: {
+    fontSize: 24,
+    marginBottom: 30,
+  },
+  input: {
+    width: '80%',
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1,
+    marginBottom: 20,
+    paddingLeft: 10,
+    borderRadius: 20,
+  },
+  button: {
+    backgroundColor: '#CB6CE6',
+    padding: 10,
+    borderRadius: 10,
+  },
+  buttonText: {
+    color: 'white',
+    fontSize: 16,
+    textAlign: 'center',
+  },
+});
+
+export default LoginScreen;
